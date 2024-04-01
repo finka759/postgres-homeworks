8d1d50a410cc5095c51c91038535d3dfc9aa72b9
finka759 <finka759@gmail.com>
2024-04-01 23:08:41 +0300

    18.1 готово

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -7,7 +7,7 @@
     SELECT DISTINCT ship_country, ship_city FROM orders ORDER BY ship_country, ship_city;
     SELECT ship_country, ship_city FROM orders GROUP BY ship_country, ship_city ORDER BY ship_country, ship_city;
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-    SELECT AVG(shipped_date - order_date) FROM  orders WHERE ship_country IN ('Germany')
+    SELECT AVG(shipped_date - order_date) FROM  orders WHERE ship_country IN ('Germany');
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
     SELECT  MIN(unit_price), MAX(unit_price) FROM products WHERE discontinued NOT IN(1);
--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -1,7 +1,7 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. "имя контакта" и "город" (contact_name, city) из таблицы customers (только эти две колонки)
 
-    SELECT contact_name, city FROM customers
+    SELECT contact_name, city FROM customers;
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
 
